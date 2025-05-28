4a2d21416cafda151c207253b9dd0ceca6cf80cc
ShreyaSuddala <shreyasuddala27@gmail.com>
2025-05-28 10:53:04 +0530

    navbar creation

---
+++ movie-ticket-application/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import styles from '../assets/styles/homepage.module.css'
+import Header from '../layouts/Header'
+
+const HomePage = () => {
+  return (
+    <div className={styles.homeContainer}>
+      <Header/>
+
+    </div>
+  )
+}
+
+export default HomePage
--- movie-ticket-application/src/routes/Routes.jsx
+++ movie-ticket-application/src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../layouts/Header';
+import HomePage from '../pages/HomePage';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/homepage' element={<HomePage/>}/>
         <Route path="/*" element={<Header />} />
       </Routes>
     </BrowserRouter>
